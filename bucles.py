39bf4f63ebb9fdfdf21c3b54b84442da82c6e387
nuriagorchs <163451803+nuriagorchs@users.noreply.github.com>
2024-04-24 20:47:28 +0200

    Add files via upload

---
+++ bucles.py
@@ -0,0 +1,21 @@
+''' reto microlearning bucles III'''
+
+numeros = [5,8,7,-3,2,1,-9,-10]
+total = 0
+
+for numero in numeros:
+    
+    if numero < 0:
+        print ("el total número negativo es:",numero)
+        
+
+   
+
+
+
+
+    
+
+        
+    
+
